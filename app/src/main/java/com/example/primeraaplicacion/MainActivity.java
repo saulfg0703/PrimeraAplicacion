08200bb55c127a6fe9dac46c75e8c5393addf172
saulfg0703 <93255995+saulfg0703@users.noreply.github.com>
2022-09-23 13:14:15 +0200

    Primercommit

---
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.primeraaplicacion;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    int contador = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void incrementarContador(View vista){
+        contador ++;
+        mostrarResultado();
+
+    }
+
+    public void decrementarContador(View vista){
+        contador --;
+        mostrarResultado();
+    }
+    public void resetearContador(View vista){
+        contador = 0;
+        mostrarResultado();
+    }
+    public void mostrarResultado(){
+        TextView resultadoTexto = (TextView)findViewById(R.id.contador);
+        resultadoTexto.setText("Contador: " + contador);
+    }
+}
