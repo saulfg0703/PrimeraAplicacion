d51b15ef9046b5ffefcfe8ef6ab6f615e2f36333
saulfg0703 <93255995+saulfg0703@users.noreply.github.com>
2022-09-29 11:35:57 +0200

    Video 15

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.primeraaplicacion;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     int contador = 0;
+    TextView textoAMostrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +28,22 @@ public class MainActivity extends Activity {
         mostrarResultado();
     }
     public void resetearContador(View vista){
-        contador = 0;
+        EditText resetearNumero =(EditText) findViewById(R.id.textoReseteo);
+        contador = Integer.parseInt(resetearNumero.getText().toString());
+        resetearNumero.setText("");
         mostrarResultado();
+
     }
     public void mostrarResultado(){
-        TextView resultadoTexto = (TextView)findViewById(R.id.contadorTexto);
-        resultadoTexto.setText(String.valueOf(contador));
+        TextView texto = (TextView)findViewById(R.id.contadorTexto);
+        texto.setText(String.valueOf(contador));
     }
+    public void conteoNegativos(View vista){
+        CheckBox validar = (CheckBox) findViewById(R.id.validar);
+        if(contador<0){
+            contador = 0;
+        }
+        mostrarResultado();
+    }
+
 }
