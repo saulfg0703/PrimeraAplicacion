a218533a3308909f56bdde8340d05fed58415999
saulfg0703 <93255995+saulfg0703@users.noreply.github.com>
2022-10-04 11:21:04 +0200

    Video 17

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,31 +22,34 @@ public class MainActivity extends Activity {
 
         contador = 0;
     }
-    @SuppressLint("SetTextI18n")
-    public void incrementarContador(View vista){
-        contador ++;
 
-        textoAMostrar.setText(""+contador);
+    @SuppressLint("SetTextI18n")
+    public void incrementarContador(View vista) {
+        contador++;
+
+        textoAMostrar.setText("" + contador);
 
     }
 
     @SuppressLint("SetTextI18n")
-    public void decrementarContador(View vista){
+    public void decrementarContador(View vista) {
         CheckBox validar = findViewById(R.id.validar);
-        contador --;
-        if(validar.isChecked()){
+        contador--;
+        if (validar.isChecked()) {
             contador = 0;
         }
-        textoAMostrar.setText(""+contador);
+        textoAMostrar.setText("" + contador);
     }
-    public void resetearContador(View vista){
+
+    public void resetearContador(View vista) {
         EditText resetearNumero = findViewById(R.id.textoReseteo);
         contador = Integer.parseInt(resetearNumero.getText().toString());
         resetearNumero.setText("");
-        textoAMostrar.setText(""+contador);
+        textoAMostrar.setText("" + contador);
+        InputMethodManager miTeclado = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        miTeclado.hideSoftInputFromWindow(resetearNumero.getWindowToken(),0);
     }
- /*   public void mostrarResultado(){
-        TextView texto = (TextView)findViewById(R.id.contadorTexto);
-        texto.setText(String.valueOf(contador));
-    }*/
+
+
+
 }
